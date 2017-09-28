6ee531215135cf8b17dc6016952fc58153a06862
Levente Páger <levente.pager@guidesmiths.com>
2017-09-28 16:45:02 +0200

    Backend works fine

--- web/src/components/Players.js
+++ web/src/components/Players.js
@@ -5,7 +5,7 @@ class Players extends Component {
     super(props);
   }
   render() {
-    const players = this.props.players.map(p => <li style={{color: p.color}} key={p.id}>{p.name} is in cell {p.position}</li>);
+      const players = this.props.players.map(p => <li style={{color: p.color}} key={p.id}>{p.name} is in cell {p.position} <span style={{color:"red"}}>{p.position >= 100? "Win":null}</span></li>);
     return <div className="players">
       <div className="opaq">
       </div>
