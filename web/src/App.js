e4fa5d786ec94dd96d64dd0d8c65feeee4730e5c
Orsolyajusztin <orsolya.jusztin@jaystack.com>
2017-09-28 12:37:12 +0200

    add starting form

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import background from './assets/background.jpg';
-import './App.css';
 import io from 'socket.io-client';
-
+import './App.css';
+import {StartingForm} from './components'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   onPress(e) {
     
   }
+  
   componentDidMount() {
     const socket = io('localhost:1367/notifications', {reconnect: true});
     socket.emit('hello', {hello: true});
   }
+
   render() {
     return (
       <div className="App">
-        <div className="yourName">
-          Enter Your Name:<br />
-          <input name="playerName" value="" />
-        </div>
+        <StartingForm />
       </div>
     );
   }
---
+++ web/src/components/index.js
@@ -0,0 +1,5 @@
+import StartingForm from './StartingForm'
+
+export {
+    StartingForm,
+}
