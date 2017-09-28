81e19ebe78e7e06999f7749b26e55668eadef206
loveirobert <loveirobert@gmail.com>
2017-09-28 13:23:33 +0200

    position added to players

--- web/src/components/Players.js
+++ web/src/components/Players.js
@@ -5,7 +5,7 @@ class Players extends Component {
     super(props);
   }
   render() {
-    const players = this.props.players.map(p => <li style={{color: p.color}} key={p.id}>{p.name}</li>);
+    const players = this.props.players.map(p => <li style={{color: p.color}} key={p.id}>{p.name} is in cell {p.position}</li>);
     return <div className="players">
       <div className="opaq">
       </div>
